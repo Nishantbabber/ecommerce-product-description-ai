ee6fd7b147cf7b97ebd49159a812074db3017826
apple <apple@apples-MacBook-Air.local>
2024-08-26 23:45:01 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -16,12 +16,9 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/user', require('./routes/user'));
