b8da9756ca407827e504bba24ff71103bdf878f8
apple <apple@apples-MacBook-Air.local>
2024-08-27 23:56:15 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -16,9 +16,10 @@ app.use(cors({
 }));
 
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://ecommerce-product-description-ai-xqjz.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(200);
 });
 
